d3347bdc4849444e69508b417b62927295ca93fc
haku1gh <haku+github@mailbox.org>
2020-03-15 15:25:10 +0100

    Add NGettext library

--- src/UltraStar.Core/GlobalSuppressions.cs
+++ src/UltraStar.Core/GlobalSuppressions.cs
@@ -4,3 +4,15 @@
 // a specific target and scoped to a namespace, type, member, etc.
 
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0018:Inline variable declaration", Justification = "Does not always make it more readable.")]
+
+// Disable warnings from NGettext
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "CS1591:Missing XML comment for publibly visible type or member", Justification = "External code.", Scope = "type", Target = "UltraStar.Core.ThirdParty.NGettext.Loaders.CatalogLoadingException")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "CS1591:Missing XML comment for publibly visible type or member", Justification = "External code.", Scope = "type", Target = "UltraStar.Core.ThirdParty.NGettext.PluralCompile.CompiledPluralRule")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "CS1591:Missing XML comment for publibly visible type or member", Justification = "External code.", Scope = "type", Target = "UltraStar.Core.ThirdParty.NGettext.PluralCompile.Compiler.PluralRuleCompiler")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "CS1591:Missing XML comment for publibly visible type or member", Justification = "External code.", Scope = "type", Target = "UltraStar.Core.ThirdParty.NGettext.Plural.AstPluralRule")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "CS1591:Missing XML comment for publibly visible type or member", Justification = "External code.", Scope = "type", Target = "UltraStar.Core.ThirdParty.NGettext.Plural.Ast.AstTokenParser")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "CS1591:Missing XML comment for publibly visible type or member", Justification = "External code.", Scope = "type", Target = "UltraStar.Core.ThirdParty.NGettext.Plural.Ast.ParserException")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "CS1591:Missing XML comment for publibly visible type or member", Justification = "External code.", Scope = "type", Target = "UltraStar.Core.ThirdParty.NGettext.Plural.Ast.Token")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "CS1591:Missing XML comment for publibly visible type or member", Justification = "External code.", Scope = "type", Target = "UltraStar.Core.ThirdParty.NGettext.Plural.Ast.TokenDefinition")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "CS1591:Missing XML comment for publibly visible type or member", Justification = "External code.", Scope = "type", Target = "UltraStar.Core.ThirdParty.NGettext.Plural.Ast.TokenType")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Stype", "CS3021:Type or member does not need a CLSCompliant attribute because the assembly does not have a CLSCompliant attribute", Justification = "External code.", Scope = "type", Target = "UltraStar.Core.ThirdParty.NGettext.Loaders.BigEndianBinaryReader")]
