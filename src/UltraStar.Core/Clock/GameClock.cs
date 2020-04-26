5dd557d803b9840f1d009bfe5970ddb903dd163f
haku1gh <haku+github@mailbox.org>
2020-04-27 00:26:44 +0200

    Created a global and game clock.

---
+++ src/UltraStar.Core/Clock/GameClock.cs
@@ -0,0 +1,158 @@
+﻿#region License (LGPL)
+/*
+ * This file is part of UltraStar.Core.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License 3
+ * along with UltraStar.Core. If not, see <http://www.gnu.org/licenses/lgpl-3.0>.
+ */
+#endregion License
+
+using System;
+using System.Diagnostics;
+
+namespace UltraStar.Core.Clock
+{
+    /// <summary>
+    /// Respresents a clock for the Ultrastar game.
+    /// </summary>
+    internal class GameClock
+    {
+        // Private variables
+        private Stopwatch watch;
+        private uint delay;
+        private long zeroOffset;
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="GameClock"/>.
+        /// </summary>
+        /// <param name="delay">
+        /// An additional delay in [us] for starting the game clock.
+        /// With this delay it is possible to get a negative <see cref="Elapsed"/> time.
+        /// </param>
+        public GameClock(uint delay = 0)
+        {
+            watch = new Stopwatch();
+            watch.Start();
+            if (delay < 0) delay = 0;
+            this.delay = delay;
+            Running = false;
+            Paused = false;
+        }
+
+        /// <summary>
+        /// Starts the game clock.
+        /// </summary>
+        /// <param name="reference">
+        /// A reference in [us] to the <see cref="GlobalClock"/> time.
+        /// This will serve as an delay calculated as: <paramref name="reference"/>-<see cref="GlobalClock.Ticks"/>.
+        /// With this reference it is possible to get a negative <see cref="Elapsed"/> time.
+        /// It is also possible to contruct a game clock that starts with an <see cref="Elapsed"/> time of greater than 0.
+        /// </param>
+        public void Start(long reference)
+        {
+            zeroOffset = delay + reference - GlobalClock.Ticks;
+            watch.Restart();
+            Running = true;
+            Paused = false;
+        }
+
+        /// <summary>
+        /// Starts the game clock.
+        /// </summary>
+        public void Start()
+        {
+            zeroOffset = delay;
+            Running = true;
+            Paused = false;
+        }
+
+        /// <summary>
+        /// Pauses the game clock.
+        /// </summary>
+        public void Pause()
+        {
+            if (Running)
+            {
+                watch.Stop();
+                Paused = true;
+            }
+        }
+
+        /// <summary>
+        /// Resumes the game clock.
+        /// </summary>
+        public void Resume()
+        {
+            if (Paused)
+            {
+                watch.Start();
+                Paused = false;
+            }
+        }
+
+        /// <summary>
+        /// Stops the game clock.
+        /// </summary>
+        public void Stop()
+        {
+            if (Running)
+            {
+                watch.Stop();
+                Running = false;
+                Paused = false;
+            }
+        }
+
+        /// <summary>
+        /// Gets the elapsed milli seconds [ms].
+        /// </summary>
+        public long ElapsedMilliseconds
+        {
+            get { return (1000 * watch.ElapsedTicks / Stopwatch.Frequency) - (zeroOffset / 1000); }
+        }
+
+        /// <summary>
+        /// Gets the elapsed micro seconds [us].
+        /// </summary>
+        public long ElapsedMicroseconds
+        {
+            get { return (1000000 * watch.ElapsedTicks / Stopwatch.Frequency) - zeroOffset; }
+        }
+
+        /// <summary>
+        /// Gets the elapsed seconds [s].
+        /// </summary>
+        public float Elapsed
+        {
+            get { return (float)ElapsedMicroseconds / 1000000; }
+        }
+
+        /// <summary>
+        /// Gets or sets the delay of game clock.
+        /// </summary>
+        /// <remarks>
+        /// Changes are only applied when the clock is restarted.
+        /// </remarks>
+        public uint Delay
+        {
+            get { return delay; }
+            set
+            {
+                if (value < 0)
+                    delay = 0;
+                else
+                    delay = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets an indicator whether the clock is running.
+        /// </summary>
+        public bool Running { get; private set; }
+
+        /// <summary>
+        /// Gets an indicator whether the clock is paused.
+        /// </summary>
+        public bool Paused { get; private set; }
+    }
+}
---
+++ src/UltraStar.Core/Clock/GlobalClock.cs
@@ -0,0 +1,49 @@
+﻿#region License (LGPL)
+/*
+ * This file is part of UltraStar.Core.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License 3
+ * along with UltraStar.Core. If not, see <http://www.gnu.org/licenses/lgpl-3.0>.
+ */
+#endregion License
+
+using System;
+using System.Diagnostics;
+
+namespace UltraStar.Core.Clock
+{
+    /// <summary>
+    /// Respresents a global clock.
+    /// </summary>
+    internal static class GlobalClock
+    {
+        private static Stopwatch watch;
+        private static long internalOffset = 0;
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="GlobalClock"/>.
+        /// </summary>
+        static GlobalClock()
+        {
+            watch = new Stopwatch();
+            watch.Start();
+        }
+
+        /// <summary>
+        /// Syncs the clock with a new offset.
+        /// </summary>
+        /// <param name="offset">The new additional offset in [ms] to apply.</param>
+        public static void Sync(long offset)
+        {
+            internalOffset += offset;
+        }
+
+        /// <summary>
+        /// Gets the number of [ms] ticks since start of the clock. 
+        /// </summary>
+        public static long Ticks
+        {
+            get { return (1000 * watch.ElapsedTicks / Stopwatch.Frequency) + internalOffset; }
+        }
+    }
+}
